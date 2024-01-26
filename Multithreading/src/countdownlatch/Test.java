262e97b94853faf0eef04b0ceb2317050de63ca9
Artem Yakkonen <iakonien@gmail.com>
2024-01-26 16:16:57 +0300

    reentrantblock, semaphore, deadlock(1/2)

--- Multithreading/src/countdownlatch/Test.java
+++ Multithreading/src/countdownlatch/Test.java
@@ -7,42 +7,32 @@ import java.util.concurrent.Executors;
 public class Test {
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(3);
+
         ExecutorService executorService = Executors.newFixedThreadPool(3);
+
         for (int i = 0; i < 3; i++) {
-            executorService.submit(new Processor(countDownLatch, i));
+            executorService.submit(new Processor(countDownLatch));
         }
         executorService.shutdown();
 
-        for (int i = 0; i < 3; i++) {
-            Thread.sleep(1000);
-            countDownLatch.countDown();
-        }
+        countDownLatch.await();
+        System.out.println("Latch has been opened, main thread is proceeding!");
     }
 }
 
 class Processor implements Runnable{
-    private int id;
     private CountDownLatch countDownLatch;
 
-    public Processor(CountDownLatch countDownLatch, int id){
+    public Processor(CountDownLatch countDownLatch){
         this.countDownLatch = countDownLatch;
-        this.id = id;
     }
-
     @Override
     public void run() {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-        System.out.println("Thread with id = " + id + " proceeded.");
+        countDownLatch.countDown();
     }
 }
