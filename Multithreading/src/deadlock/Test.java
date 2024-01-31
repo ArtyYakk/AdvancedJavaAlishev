62880de7b54b9d2f0380921f84a7d1cc10bc5319
Artem Yakkonen <iakonien@gmail.com>
2024-01-31 16:01:44 +0300

    deadlock, thread's interruption, callable and future

--- Multithreading/src/deadlock/Test.java
+++ Multithreading/src/deadlock/Test.java
@@ -1,6 +1,8 @@
 package deadlock;
 
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
     public static void main(String[] args) throws InterruptedException {
@@ -33,17 +35,50 @@ class Runner{
 
     private Account account1 = new Account();
     private Account account2 = new Account();
+    private Lock lock1 = new ReentrantLock();
+    private Lock lock2 = new ReentrantLock();
+    private void takeLocks(Lock lock1, Lock lock2){
+        boolean firstLockTaken = false;
+        boolean secondLockTaken = false;
 
+        while (true){
+            try{
+                firstLockTaken = lock1.tryLock();
+                secondLockTaken = lock2.tryLock();
+            } finally {
+                if(firstLockTaken && secondLockTaken){
+                    return;
+                }
+
+                if(firstLockTaken){
+                    lock1.unlock();
+                }
+
+                if(secondLockTaken){
+                    lock2.unlock();
+                }
+            }
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 
     public void firstThread(){
         Random random = new Random();
 
         for (int i = 0; i < 10_000; i++) {
-            synchronized (account1){
-                synchronized (account2){
-                    Account.transfer(account1, account2, random.nextInt(100));
-                }
+            takeLocks(lock1, lock2);
+            try{
+                Account.transfer(account1, account2, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
             }
         }
     }
@@ -53,10 +88,12 @@ class Runner{
         Random random = new Random();
 
         for (int i = 0; i < 10_000; i++) {
-            synchronized (account1){
-                synchronized (account2){
-                    Account.transfer(account2, account1, random.nextInt(100));
-                }
+            takeLocks(lock2, lock1);
+            try{
+                Account.transfer(account2, account1, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
             }
         }
     }
