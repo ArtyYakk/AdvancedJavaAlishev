04c6fccdc0efe1482964f16d1ec1a05507b09a3c
iakon <iakonien@gmail.com>
2024-02-11 00:59:40 +0300

    added mvn and started junit

---
+++ JUnit/src/test/java/Vector2DTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class Vector2DTest {
+    @Test
+    public void newVectorShouldHaveZeroLength(){
+        Vector2D v1 = new Vector2D(); // action
+        // assertion
+       // Assert.assert
+
+    }
+}
